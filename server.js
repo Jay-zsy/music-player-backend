b8b116cb84449912f4e7d5b4b881463adec0993b
Jay Zhou <hollowic@hotmail.com>
2020-05-18 18:16:06 -0700

    Aux folder

--- server.js
+++ server.js
@@ -1,11 +1,3 @@
-/**
- * This is an example of a basic node.js script that performs
- * the Authorization Code oAuth2 flow to authenticate against
- * the Spotify Accounts.
- *
- * For more information, read
- * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
- */
 require("dotenv").config();
 const express = require("express"); // Express web server framework
 const axios = require("axios");
@@ -16,6 +8,7 @@ const querystring = require("querystring");
 const cookieParser = require("cookie-parser");
 const cookieSession = require("cookie-session");
 const cheerio = require("cheerio");
+const { generateRandomString } = require("./aux/helpers");
 
 const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID; // Your spotify client id
 const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET; // Your spotify secret
@@ -26,21 +19,6 @@ const GENIUS_CALLBACK_URI = process.env.GENIUS_CALLBACK_URI; // Your genius redi
 const FRONTEND_CALLBACK_URL = process.env.FRONTEND_CALLBACK_URL; // Your frontend uri
 const SCOPE = process.env.SCOPE;
 const KEYS = process.env.KEYS;
-/**
- * Generates a random string containing numbers and letters
- * @param  {number} length The length of the string
- * @return {string} The generated string
- */
-let generateRandomString = function (length) {
-  let text = "";
-  let possible =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-  for (let i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
-};
 
 let stateKey = "spotify_auth_state";
 
@@ -57,46 +35,23 @@ app.use(function (req, res, next) {
   );
   next();
 });
-// const whitelist = [
-//   "https://my-personal-music-player.netlify.app",
-//   "http://localhost/3000",
-// ];
-// const corsOptions = {
-//   credentials: true,
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-// };
-
-const corsOptions = {
-  credentials: true,
-  origin: "*",
-};
 
 app
-  // .use(
-  //   cookieSession({
-  //     name: "session",
-  //     keys: [KEYS],
-
-  //     // Cookie Options
-  //     maxAge: 60 * 60 * 1000, // 1 hours
-  //   })
-  // )
   .use(express.static(__dirname + "/build"))
-  .use(cors(corsOptions))
+  .use(cors())
   .use(cookieParser())
   .use(morgan("dev"));
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.send("I am a teapot");
 });
 
-app.get("/spotify", function (req, res) {
+app.get("/api/abc", (req, res) => {
+  console.log(req.headers);
+  res.json({ data: "cba" });
+});
+
+app.get("/api/spotify", function (req, res) {
   let state = generateRandomString(16);
   res.cookie(stateKey, state);
   // your application requests authorization
@@ -151,27 +106,14 @@ app.get("/api/spotifycallback", function (req, res) {
         const refresh_token = response.data.refresh_token;
         // res.cookie("spotify_access_token", access_token);
         // res.cookie("spotify_refresh_token", refresh_token);
-
-        axios({
-          method: "get",
-          url: "https://api.spotify.com/v1/me",
-          headers: { Authorization: "Bearer " + access_token },
-        })
-          .then(() => {
-            res.redirect(
-              FRONTEND_CALLBACK_URL +
-                "/#" +
-                querystring.stringify({
-                  access_token: access_token,
-                  refresh_token: refresh_token,
-                })
-            );
-          })
-          .catch((e) => {
-            res.redirect(
-              "/#" + querystring.stringify({ error: e.response.data })
-            );
-          });
+        res.redirect(
+          FRONTEND_CALLBACK_URL +
+            "/#" +
+            querystring.stringify({
+              access_token: access_token,
+              refresh_token: refresh_token,
+            })
+        );
       })
       .catch((e) => console.error(e.response.data));
   }
@@ -209,160 +151,6 @@ app.get("/api/refresh_token", function (req, res) {
       console.log(e);
     });
 });
-
-// app.get("/genius", function (req, res) {
-//   // your application requests authorization
-//   let state = generateRandomString(16);
-//   let scope = "me create_annotation manage_annotation vote";
-//   res.redirect(
-//     "https://api.genius.com/oauth/authorize?" +
-//       querystring.stringify({
-//         response_type: "code",
-//         client_id: GENIUS_CLIENT_ID,
-//         scope,
-//         redirect_uri: GENIUS_CALLBACK_URI,
-//         state,
-//       })
-//   );
-// });
-// app.get("/geniuscallback", function (req, res) {
-//   // your application requests refresh and access tokens
-//   // after checking the state parameter
-//   let code = req.query.code || null;
-//   let state = req.query.state || null;
-//   if (state === null) {
-//     res.redirect(
-//       "/#" +
-//         querystring.stringify({
-//           error: "state_mismatch",
-//         })
-//     );
-//   } else {
-//     res.clearCookie(stateKey);
-//     let authOptions = {
-//       url: "https://api.genius.com/oauth/token",
-//       form: {
-//         code: code,
-//         redirect_uri: GENIUS_CALLBACK_URI,
-//         grant_type: "authorization_code",
-//       },
-//       headers: {
-//         Authorization:
-//           "Basic " +
-//           new Buffer.from(
-//             GENIUS_CLIENT_ID + ":" + GENIUS_CLIENT_SECRET
-//           ).toString("base64"),
-//       },
-//       json: true,
-//     };
-//     console.log(authOptions);
-//     axios
-//       .post(authOptions, function (error, response, body) {
-//         if (!error && response.statusCode === 200) {
-//           console.log(body);
-//           let access_token = body.access_token;
-//           console.log("Got the access token!", access_token);
-//           res.cookie("genius_access_token", access_token);
-//           res.redirect(FRONTEND_CALLBACK_URL);
-//         } else {
-//           res.redirect(
-//             FRONTEND_CALLBACK_URL +
-//               "?error=" +
-//               querystring.stringify({
-//                 error: "invalid_token",
-//               })
-//           );
-//         }
-//       })
-//       .catch((err) => console.log(err.config.data));
-//   }
-// });
-
-// app.post("/lyrics", (req, res) => {
-//   const token = req.body.token;
-//   const trackName = req.body.trackName;
-//   const trackArtist = req.body.trackArtist;
-//   const firstCallOptions = {
-//     method: "GET",
-//     url: `https://api.genius.com/search?q=${encodeURI(trackName)}%20${encodeURI(
-//       trackArtist
-//     )}`,
-//     headers: {
-//       Authorization: "Bearer " + token,
-//     },
-//   };
-//   const secondCallOptions = {
-//     method: "GET",
-//     url: `https://api.genius.com/search?q=${encodeURI(
-//       trackName.split("(")[0].split("-")[0]
-//     )}%20${encodeURI(trackArtist)}`,
-//     headers: {
-//       Authorization: "Bearer " + token,
-//     },
-//   };
-//   axios.get(firstCallOptions, (error, response, body) => {
-//     const firstDataArr = JSON.parse(body).response.hits;
-//     //Check to see if there are any results
-//     if (firstDataArr.length === 0) {
-//       res.status(404).send("No lyrics available");
-//       return;
-//     }
-//     //Second check for track name and artist match
-//     if (
-//       !firstDataArr[0].result.full_title
-//         .toUpperCase()
-//         .includes(trackName.toUpperCase()) &&
-//       !firstDataArr[0].result.full_title
-//         .toUpperCase()
-//         .includes(trackArtist.toUpperCase())
-//     ) {
-//       //make another call without the parentheses
-//       axios.get(secondCallOptions, (error, response, body) => {
-//         const secondDataArr = JSON.parse(body).response.hits;
-//         if (secondDataArr.length === 0) {
-//           res.status(404).send("No lyrics available");
-//           return;
-//         }
-//         if (
-//           !secondDataArr[0].result.full_title
-//             .toUpperCase()
-//             .includes(trackName.split("(")[0].split("-")[0].toUpperCase()) &&
-//           !secondDataArr[0].result.full_title
-//             .toUpperCase()
-//             .includes(trackArtist.toUpperCase())
-//         ) {
-//           res.status(404).send("No lyrics available");
-//           return;
-//         }
-//         const URL = secondDataArr[0].result.url;
-//         axios.get(URL, (error, response, body) => {
-//           if (!error && res.statusCode === 200) {
-//             const $ = cheerio.load(body);
-//             const lyrics = $(".lyrics").text();
-//             res.status(200).send(lyrics);
-//             return;
-//           } else {
-//             res.status(418).send("No lyrics available");
-//             return;
-//           }
-//         });
-//       });
-//     } else {
-//       const URL = firstDataArr[0].result.url;
-//       axios.get(URL, (error, response, body) => {
-//         if (!error && res.statusCode === 200) {
-//           const $ = cheerio.load(body);
-//           const lyrics = $(".lyrics").text();
-//           res.status(200).send(lyrics);
-//           return;
-//         } else {
-//           res.status(418).send("No lyrics available");
-//           return;
-//         }
-//       });
-//     }
-//   });
-// });
 
 console.log("Listening on 8888");
 app.listen(PORT);
